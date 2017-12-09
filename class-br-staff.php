af7707c0f396a90df8edbf2ebfd8ff202a5e1fa7
brothman01 <brothman01@gmail.com>
2017-12-09 13:23:03 -0500

    Add files via upload
    - Has staff member post type with bibliographic fields for the member.
    - to add a staff member just add a new post to the table, fill out the info and add a picture.
    - To add a staff member row to a page, copy the shortcode in the post table next to the staff member you want to add and paste it into any page.

---
+++ class-br-staff.php
@@ -0,0 +1,110 @@
+<?php
+/*
+Plugin Name: Staff-brothman
+Plugin URI: http://wordpress.org/extend/plugins/wordpress-importer/
+Description: Import posts, pages, comments, custom fields, categories, tags and more from a WordPress export file.
+Author: wordpressdotorg
+Author URI: http://wordpress.org/
+Version: 0.6.3
+Text Domain: wordpress-importer
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+class Br_Staff {
+
+	public function __construct() {
+
+		/* Register Post Types */
+		require_once( 'post-types/br-person.php' );
+
+		// Enqueue CMB2
+		if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+			require_once dirname( __FILE__ ) . '/cmb2/init.php';
+		} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+			require_once dirname( __FILE__ ) . '/CMB2/init.php';
+		}
+
+		// add custom row to table
+		add_filter( 'manage_posts_columns', [ $this, 'brs_columns_head' ] );
+		add_action( 'manage_posts_custom_column', [ $this, 'brs_columns_content' ], 10, 2 );
+
+		add_shortcode( 'br_person', [ $this, 'br_person_shortcode_function' ] );
+
+		add_action( 'wp_enqueue_scripts', [ $this, 'brs_enqueue_styles' ] );
+	}
+
+	/**
+ * [br_person_shortcode_function description]
+ * @param  array $atts Is an array although only item of the array is the id of the staff member to use.
+ * @return [type]       [description]
+ */
+	public function br_person_shortcode_function( $atts ) {
+
+		$attributes = shortcode_atts( array(
+			'id'  => false,
+		), $atts );
+
+		$contents = '';
+
+		// The Query
+		$the_query = new WP_Query( [
+			'post_type' => 'br_person',
+			'p' => (int) $atts['id'],
+		]);
+
+		// The Loop
+		if ( $the_query->have_posts() ) {
+
+			while ( $the_query->have_posts() ) {
+				$the_query->the_post();
+				$contents .= '<div class="staff-member-div">
+								<div class="span4">
+									<img class="staff-portrait" src="' . get_post_meta( get_the_ID(), 'br_portrait', true ) . '" />
+									<p class="title-text">' . get_post_meta( get_the_ID(), 'br_title', true ) . '</p>
+								</div>
+
+				<div class="span8">
+						<div class="name-text">' . get_post_meta( get_the_ID(), 'br_name', true ) . '
+						<div class="cool-underline" style="width: 40%;"><div style="background: #E6E6E6; width: 80%; height: 4px; position: relative; bottom: 0%; margin-left: 20%;"></div></div></div>
+						 <div class="bio-text">' . get_post_meta( get_the_ID(), 'br_bio', true ) . '</div>
+				</div>
+
+				</div>';
+			}
+			/* Restore original Post Data */
+			wp_reset_postdata();
+		} else {
+			// no posts found
+		}
+
+		 return $contents;
+	}
+
+	 // ADD NEW COLUMN
+	public function brs_columns_head( $defaults ) {
+
+		if ( 'br_person' == get_post_type() ) {
+			 $defaults['shortcode'] = 'Shortcode';
+		}
+			return $defaults;
+	}
+
+	 // SHOW THE FEATURED IMAGE
+	public function brs_columns_content( $column_name, $post_id ) {
+		if ( $shortcode == $column_name ) {
+				echo '[br_person id=' . $post_id . ']';
+		}
+	}
+
+
+	/*
+	 * Load stylesheets, etc.
+	 */
+	public function brs_enqueue_styles() {
+
+		wp_enqueue_style( 'main-style', plugin_dir_url( __FILE__ ) . 'library/css/style.css' );
+
+	}
+
+}
+
+$staff = new Br_Staff();
---
+++ post-types/br-person.php
@@ -0,0 +1,137 @@
+<?php
+function custom_post_type() {
+
+		$labels = array(
+			'name'                  => _x( 'Staff Members', 'Post Type General Name', 'text_domain' ),
+			'singular_name'         => _x( 'Staff', 'Post Type Singular Name', 'text_domain' ),
+			'menu_name'             => __( 'Staff', 'text_domain' ),
+			'name_admin_bar'        => __( 'Staff', 'text_domain' ),
+			'archives'              => __( 'Featured Archives', 'text_domain' ),
+			'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+			'all_items'             => __( 'All Posts', 'text_domain' ),
+			'add_new_item'          => __( 'Add New', 'text_domain' ),
+			'add_new'               => __( 'Add New', 'text_domain' ),
+			'new_item'              => __( 'New Person', 'text_domain' ),
+			'edit_item'             => __( 'Edit Person', 'text_domain' ),
+			'update_item'           => __( 'Update Person', 'text_domain' ),
+			'view_item'             => __( 'View Person', 'text_domain' ),
+			'search_items'          => __( 'Search People', 'text_domain' ),
+			'not_found'             => __( 'Not found', 'text_domain' ),
+			'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+			'featured_image'        => __( 'Person', 'text_domain' ),
+			'set_featured_image'    => __( 'Set Person', 'text_domain' ),
+			'remove_featured_image' => __( 'Remove Person', 'text_domain' ),
+			'use_featured_image'    => __( 'Use as Person', 'text_domain' ),
+			'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+			'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+			'items_list'            => __( 'Items list', 'text_domain' ),
+			'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+			'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+		);
+		$args = array(
+			'label'                 => __( 'Post Type', 'text_domain' ),
+			'description'           => __( 'Post Type Description', 'text_domain' ),
+			'labels'                => $labels,
+			'supports'              => array(),
+			'taxonomies'            => array(),
+			'hierarchical'          => false,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_in_menu'          => true,
+			'menu_position'         => 5,
+			'show_in_admin_bar'     => true,
+			'show_in_nav_menus'     => true,
+			'can_export'            => true,
+			'has_archive'           => true,
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => true,
+			'capability_type'       => 'page',
+			'menu_icon'                => 'dashicons-admin-users',
+		);
+		register_post_type( 'br_person', $args );
+
+}
+add_action( 'init', 'custom_post_type', 0 );
+
+
+add_action( 'init', 'init_remove_support', 100 );
+function init_remove_support() {
+		remove_post_type_support( 'br_person', 'editor' );
+}
+
+add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes' );
+/**
+ * Define the metabox and field configurations.
+ */
+function cmb2_sample_metaboxes() {
+		$prefix = 'br_';
+
+		/**
+		 * Initiate the metabox
+		 */
+		$cmb = new_cmb2_box( array(
+				'id'            => 'test_metabox',
+				'title'         => __( 'Staff Member', 'cmb2' ),
+				'object_types'  => array('br_person' ), // Post type
+				'context'       => 'normal',
+				'priority'      => 'high',
+				'show_names'    => true, // Show field names on the left
+				// 'cmb_styles' => false, // false to disable the CMB stylesheet
+				// 'closed'     => true, // Keep the metabox closed by default
+		) );
+
+		// Regular text field
+		$cmb->add_field( array(
+				'name'       => __( 'Display Name', 'cmb2' ),
+				'desc'       => __( '', 'cmb2' ),
+				'id'         => $prefix . 'name',
+				'type'       => 'text',
+				'show_on_cb' => 'cmb2_hide_if_no_cats', // function should return a bool value
+				// 'sanitization_cb' => 'my_custom_sanitization', // custom sanitization callback parameter
+				// 'escape_cb'       => 'my_custom_escaping',  // custom escaping callback parameter
+				// 'on_front'        => false, // Optionally designate a field to wp-admin only
+				// 'repeatable'      => true,
+		) );
+
+		$cmb->add_field( array(
+			'name' => esc_html__( 'Portrait', 'cmb2' ),
+			'desc' => esc_html__( 'Upload an image or enter a URL.', 'cmb2' ),
+			'id'   => $prefix . 'portrait',
+			'type' => 'file',
+		) );
+
+		// Regular text field
+		$cmb->add_field( array(
+				'name'       => __( 'Title', 'cmb2' ),
+				'desc'       => __( '', 'cmb2' ),
+				'id'         => $prefix . 'title',
+				'type'       => 'text',
+				'show_on_cb' => 'cmb2_hide_if_no_cats', // function should return a bool value
+				// 'sanitization_cb' => 'my_custom_sanitization', // custom sanitization callback parameter
+				// 'escape_cb'       => 'my_custom_escaping',  // custom escaping callback parameter
+				// 'on_front'        => false, // Optionally designate a field to wp-admin only
+				// 'repeatable'      => true,
+		) );
+
+		$cmb->add_field( array(
+			'name'    => esc_html__( 'Bio', 'cmb2' ),
+			'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
+			'id'      => $prefix . 'bio',
+			'type'    => 'wysiwyg',
+			'options' => array( 'textarea_rows' => 5, ),
+		) );
+
+		$cmb->add_field( array(
+				'name'       => __( 'Shortcode', 'cmb2' ),
+				'desc'       => __( '', 'cmb2' ),
+				'id'         => $prefix . 'shortcode',
+				'type'       => 'text',
+				'show_on_cb' => 'cmb2_hide_if_no_cats', // function should return a bool value
+				'default' => 'prefix_set_test_default',
+		) );
+
+}
+
+function prefix_set_test_default( $field_args, $field ) {
+	return '[br_person id="' . $field->object_id . '"]';
+}
