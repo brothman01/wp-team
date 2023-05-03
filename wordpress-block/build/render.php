ff3506e8fcff2124ea5300070343566a8241a745
Ben Rothman <Ben@BenRothman.org>
2023-05-03 12:31:37 -0400

    commented the javascript code.

--- wordpress-block/build/render.php
+++ wordpress-block/build/render.php
@@ -1,22 +1,26 @@
 <p <?php echo get_block_wrapper_attributes(); ?>>
-
-<script>
-  function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-</script>
 <div id="staff-block-vanilla" style="overflow: hidden; max-width: 100%!important">
+
   <script>
+  // Get each staff member created on the WP dashboard with the REST API and parse the data into a usable array
   const staff = JSON.parse( httpGet('http://dev.local/wp-json/wp/v2/br_person') );
-  const block = document.getElementById('staff-block-vanilla');
+
+  // Sort each staff member so that they are ordered to appear on the page alphabetically by the slug
+  staff.sort(function(a, b){
+    if(a.slug < b.slug) { return -1; }
+    if(a.slug > b.slug) { return 1; }
+      return 0;
+    });
+
+  // generate the code for each of the rows using the data retrieved for each staff member
   let theCode = '';
   staff.forEach(element => theCode += createRow(element));
+
+  // Add the resulting code to the block
+  const block = document.getElementById('staff-block-vanilla');
   block.innerHTML = theCode;
 
+// function to generate a row to display in the block for each staff member
 function createRow(item) {
   let thePermalink = item.link;
   let theName = item.cmb2.custom_fields.br_name;
@@ -42,6 +46,15 @@ function createRow(item) {
 
   return theRow;
 }
+
+  // function to retrieve data from the WordPress REST API
+  function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+  }
 </script>
+
 </div>
 </p>
