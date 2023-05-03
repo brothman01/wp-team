1998d6bee7dd62161aa1c4a61d93475d68242a54
Ben Rothman <Ben@BenRothman.org>
2023-05-03 12:12:36 -0400

    Fixed part of wo block editor block.

--- wordpress-block/build/index.asset.php
+++ wordpress-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '9e2cf9987db016c85b49');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '22cd8c145eed29626eac');
