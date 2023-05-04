aff8ff7c28a5294150c6832aea3cc462e5257fd4
Ben Rothman <Ben@BenRothman.org>
2023-05-04 12:57:59 -0400

    working on react block.

--- wordpress-block-react/build/index.js
+++ wordpress-block-react/build/index.js
@@ -22,16 +22,60 @@ class App extends (react__WEBPACK_IMPORTED_MODULE_1___default().Component) {
   constructor(props) {
     super(props);
     this.state = {
-      clicks: 0
+      posts: []
     };
   }
-  toggle = () => {
-    this.setState(prevState => ({
-      clicks: prevState.clicks + 1
+  componentDidMount() {
+    // Fetch the data from the URL
+    const theUrl = window.location.origin + "/wp-json/wp/v2/br_person?filter[orderby]=date&order=desc&per_page=5&post_status=published";
+    fetch(theUrl).then(response => response.json()).then(response =>
+    // set the posts to the state variable 'posts' in the second then()
+    this.setState({
+      posts: response
     }));
+  }
+  createRows = () => {
+    // declare the state variable as a constant
+    const {
+      posts
+    } = this.state;
+
+    // check if posts exists and has a non-zero length
+    if (posts && posts.length) {
+      // declare k for the key outside of the loop
+      let k = 1;
+      const listItems = posts.map((post, index) => this.createRow(post));
+      return listItems;
+    }
+    return true;
   };
+
+  // function to generate a row to display in the block for each staff member
+  createRow(item) {
+    let thePermalink = item.link;
+    let theName = item.cmb2.custom_fields.br_name;
+    let theBio = item.cmb2.custom_fields.br_bio;
+    let thePortrait = item.cmb2.custom_fields.br_portrait;
+    let theTitle = item.cmb2.custom_fields.br_title;
+    let theRow = `<div class="staff-member-div" style="float:left; width: 100%">
+							<a href="` + thePermalink + `">
+								<div style="float: left;">
+									<img class="staff-portrait" src="` + thePortrait + `" style="width: 124px; margin: 0px auto" />
+									<br />
+									<p class="title-text" style="padding: 0px 0px 0px 0px!important; text-align: center;">` + theTitle + `</p>
+								</div>
+							</a>
+
+				<div style="float: left;">
+						<div class="name-text"><b>` + theName + `</b></div>
+						<div class="bio-text">` + theBio + `</div>
+				</div>
+
+				</div>`;
+    return theRow;
+  }
   render() {
-    return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("div", null, "foobaraa");
+    return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("div", null, this.createRows());
   }
 }
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (App);
