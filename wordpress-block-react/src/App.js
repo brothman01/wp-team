389ee31f85b53fb207ba7b0193ea719cdad5e2aa
Ben Rothman <Ben@BenRothman.org>
2023-05-04 13:14:13 -0400

    added comments to react block.

--- wordpress-block-react/src/App.js
+++ wordpress-block-react/src/App.js
@@ -51,21 +51,22 @@ createRow(item) {
   let thePortrait = item.cmb2.custom_fields.br_portrait;
   let theTitle = item.cmb2.custom_fields.br_title;
 
-  let theRow = `<div class="staff-member-div" style="float:left; width: 100%">
-							<a href="` + thePermalink + `">
+  // create the row for the post using the data entered into the fields on the dashboard \\
+  let theRow = <div class="staff-member-div" style="float:left; width: 100%">
+							<a href="{thePermalink}">
 								<div style="float: left;">
-									<img class="staff-portrait" src="` + thePortrait + `" style="width: 124px; margin: 0px auto" />
+									<img class="staff-portrait" src="{thePortrait}" style="width: 124px; margin: 0px auto" />
 									<br />
-									<p class="title-text" style="padding: 0px 0px 0px 0px!important; text-align: center;">` + theTitle + `</p>
+									<p class="title-text" style="padding: 0px 0px 0px 0px!important; text-align: center;">{theTitle}</p>
 								</div>
 							</a>
 
 				<div style="float: left;">
-						<div class="name-text"><b>` + theName + `</b></div>
-						<div class="bio-text">` + theBio + `</div>
+						<div class="name-text"><b>{theName}</b></div>
+						<div class="bio-text">{theBio}</div>
 				</div>
 
-				</div>`;
+				</div>;
 
   return theRow;
 }
